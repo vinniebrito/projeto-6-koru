2942c17d10b14ffdc5b08e2bf0767c0361e683cb
vnnbrt <vinniebritos@gmail.com>
2025-08-06 22:13:15 -0300

    feat:  função de envio de perguntas à IA

--- js/main.js
+++ js/main.js
@@ -10,13 +10,55 @@ function mostrarResposta() {
   respostaSection.style.display = "block";
 }
 
-// contador de caraceters
+// Função para contar caracteres - Yasmin
+let textarea = document.querySelector("textarea"),
+  character_in = document.querySelector("p span");
 
-let textarea= document.querySelector('textarea'),
-    character_in=document.querySelector('p span');
+character_in.innerText = 0;
 
-    character_in.innerText=500
-    
-    textarea.onkeyup=function(){
-        character_in.innerText=this.value.length
+textarea.onkeyup = function () {
+  character_in.innerText = this.value.length;
+};
+
+// Função para enviar pergunta à IA - Vinnie
+async function enviarPergunta() {
+  const apiKey = document.getElementById("apiKeyInput").value.trim();
+  const pergunta = document.getElementById("perguntaInput").value.trim();
+  const respostaTexto = document.getElementById("respostaTexto");
+
+  // normalmente a estrutura de loading pode ser adicionada aqui
+
+  const resposta = await fetch(
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-goog-api-key": apiKey,
+      },
+      body: JSON.stringify({
+        contents: [
+          {
+            parts: [
+              {
+                text: pergunta,
+              },
+            ],
+          },
+        ],
+      }),
     }
+  );
+
+  const dados = await resposta.json();
+  mostrarResposta();
+
+  if (!resposta.ok) {
+    respostaTexto.textContent =
+      "Erro: " + (dados.error?.message || "Erro desconhecido");
+    return;
+  }
+
+  // aqui é onde consome a resposta da IA e exibe na tela
+  // exemplo: respostaTexto.textContent = dados.(caminho da resposta), pode verificar o objeto com um console.log(dados) para ver o caminho correto. é bom usar '?' para evitar erros de sintaxe, assim se a resposta não existir (for null, undefined, etc), não quebra o código. por exemplo: dados.candidates?. e assim vai
+}
