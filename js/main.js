ca58c52adbf2b525dbf8cee41b299ff3c58ea69a
vnnbrt <vinniebritos@gmail.com>
2025-08-18 15:24:44 -0300

    refactor: corrige conflito

--- js/main.js
+++ js/main.js
@@ -234,47 +234,6 @@ function removeTecla(event) {
   );
 }
 
-// Função para exibir a pergunta - Willian
-function exibirPergunta() {
-  let pergunta = document.getElementById("perguntaInput").value;
-  document.getElementById("perguntaTexto").innerText = pergunta;
-}
-
-// Clique no botão
-document.getElementById("button-pergunta").addEventListener("click", () => {
-  exibirPergunta();
-});
-
-//secao de atalho teclas Ctrl+Enter para enviar perguntas - Bianca
-let teclasPressionadas = [];
-
-document.addEventListener("keydown", verificaAtalho);
-document.addEventListener("keyup", removeTecla);
-
-function verificaAtalho(event) {
-  const chaveTecla = event.key;
-
-  if (
-    teclasPressionadas.length == 1 &&
-    teclasPressionadas[0] == "Control" &&
-    chaveTecla == "Enter"
-  ) {
-    teclasPressionadas = [];
-    exibirPergunta();
-    enviarPergunta();
-  } else {
-    teclasPressionadas.push(chaveTecla);
-  }
-}
-
-function removeTecla(event) {
-  const chaveTecla = event.key;
-
-  teclasPressionadas = teclasPressionadas.filter(
-    (tecla) => tecla != chaveTecla
-  );
-}
-
 function abrirModal() {
   const modal = document.getElementById("modal");
   if (modal) modal.classList.add("show");
